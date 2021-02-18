d9e68118d0caafd1ebc2c706a7653aa8a32f5460
Hugo Mendes <41150642+hugostyles@users.noreply.github.com>
2021-02-18 12:32:01 +0000

    ok

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
-from aws_cdk import core
+from aws_cdk import (
+    core
+)
 
 from aws_python_topic.aws_python_topic_stack import AwsPythonTopicStack
+from aws_python_topic.aws_python_topic_stack import RDSStack
 
 
 app = core.App()
-AwsPythonTopicStack(app, "aws-python-topic", env={'region': 'us-west-2'})
+AwsPythonTopicStack(app, "aws-python-topic", env={'region': 'eu-west-2'})
+RDSStack(app, "aws-python-rds", env={'region': 'eu-west-2'})
 
 app.synth()
--- aws_python_topic/aws_python_topic_stack.py
+++ aws_python_topic/aws_python_topic_stack.py
@@ -3,7 +3,9 @@ from aws_cdk import (
     aws_sqs as sqs,
     aws_sns as sns,
     aws_sns_subscriptions as subs,
-    core
+    aws_ec2 as ec2,
+    aws_rds as rds,
+     core
 )
 
 
@@ -22,3 +24,25 @@ class AwsPythonTopicStack(core.Stack):
         )
 
         topic.add_subscription(subs.SqsSubscription(queue))
+
+class RDSStack(core.Stack):
+    def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        vpc = ec2.Vpc(self, "VPC")
+
+        rds.DatabaseInstance(
+            self, "RDS",
+            database_name="db1",
+            engine=rds.DatabaseInstanceEngine.mysql(
+                version=rds.MysqlEngineVersion.VER_8_0_16
+            ),
+            vpc=vpc,
+            port=3306,
+            instance_type= ec2.InstanceType.of(
+                ec2.InstanceClass.MEMORY4,
+                ec2.InstanceSize.LARGE,
+            ),
+            removal_policy=core.RemovalPolicy.DESTROY,
+            deletion_protection=False
+        )
